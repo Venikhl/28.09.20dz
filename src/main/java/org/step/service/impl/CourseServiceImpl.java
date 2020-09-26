c4793e3e132aecc3ffcfd6f2146c7a629a28b210
Gleb <asd>
2020-09-26 14:26:46 +0600

    asdf

---
+++ src/main/java/org/step/service/impl/CourseServiceImpl.java
@@ -0,0 +1,45 @@
+package org.step.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.step.entity.Course;
+import org.step.repository.CrudRepository;
+import org.step.service.CrudService;
+
+import java.util.List;
+
+@Service
+public class CourseServiceImpl implements CrudService<Course> {
+
+    private final CrudRepository<Course> courseCrudRepository;
+
+    @Autowired
+    public CourseServiceImpl(CrudRepository<Course> courseCrudRepository) {
+        this.courseCrudRepository = courseCrudRepository;
+    }
+
+    @Override
+    @Transactional
+    public void deleteAll() {
+        courseCrudRepository.deleteAll();
+    }
+
+    @Override
+    @Transactional
+    public Course save(Course course) {
+        return courseCrudRepository.save(course);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Course> findAll() {
+        return courseCrudRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        courseCrudRepository.delete(id);
+    }
+}
